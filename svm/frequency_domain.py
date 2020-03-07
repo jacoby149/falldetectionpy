5f9a9eb534cc0b6b28e5e4c5f51b41f7acfe18cb
jacoby149 <jackstudent149@gmail.com>
2020-03-06 21:18:55 -0500

    messin with frequency stuff

--- svm/frequency_domain.py
+++ svm/frequency_domain.py
@@ -2,9 +2,9 @@
 from scipy import signal
 
 def STFT():
-    #signal.stft()
+    signal.stft()
     return
     
 def Wavelet()
-    #signal.cwt(data,signal.ricker,[1,2,4,8])
+    signal.cwt(data,signal.ricker,[1,2,4,8])
     return
