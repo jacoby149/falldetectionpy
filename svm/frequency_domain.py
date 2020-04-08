3a549b5bed1808b24de6e76803e3bab959957f2a
Sojeong Lee <sojeongl@andrew.cmu.edu>
2020-04-08 11:42:07 -0400

    merge with frequency features and clean up code

--- svm/frequency_domain.py
+++ svm/frequency_domain.py
@@ -4,18 +4,18 @@ from load_data import *
 from matplotlib import pyplot as plt
 import numpy as np
 
-col = "magnitude"
-forward_fall = load_data_from_csv("./data/forward_fall.csv",col)
-side_fall = load_data_from_csv("./data/s_fall.csv",col)
-walking = load_data_from_csv("./data/walking.csv",col)
-jumping = load_data_from_csv("./data/jumping.csv",col)
+# col = "magnitude"
+# forward_fall = load_data_from_csv("./data/forward_fall.csv",col)
+# side_fall = load_data_from_csv("./data/s_fall.csv",col)
+# walking = load_data_from_csv("./data/walking.csv",col)
+# jumping = load_data_from_csv("./data/jumping.csv",col)
 RESOLUTION = 2**3 - 1
 
 """ WAVELET WORKS BETTER, CAPTURES TEMPORAL CHANGES
 def STFT(data):
     for d in data:
-        f, t, Zxx = signal.stft(d, fs=1.0, window='hann', nperseg=256, 
-            noverlap=None, nfft=None, detrend=False, return_onesided=True, 
+        f, t, Zxx = signal.stft(d, fs=1.0, window='hann', nperseg=256,
+            noverlap=None, nfft=None, detrend=False, return_onesided=True,
                 boundary='zeros', padded=True, axis=-1)
         plt.pcolormesh(t, f, np.abs(Zxx), vmin=0, vmax=1)
         plt.title('STFT Magnitude')
@@ -27,43 +27,43 @@ def STFT(data):
 def mag(data):
     for d in data:
         plt.plot(d)
-        plt.show()  
+        plt.show()
 
-    
+
 def Wavelet(data):
     answer = []
-    for d in data:
-        widths = np.arange(1, RESOLUTION)
-        cwtmatr = signal.cwt(d, signal.ricker, widths)
-        answer.append(cwtmatr)
-    return answer
-        
-    
+    # for d in data:
+    widths = np.arange(1, RESOLUTION)
+    cwtmatr = signal.cwt(data, signal.ricker, widths)
+    # answer.append(cwtmatr)
+    return cwtmatr
+
+
 def plot_wavlet(data):
         results = Wavelet(data)
         for mats in results:
             plt.imshow(mats, extent=[-1, 1, 1, RESOLUTION], cmap='PRGn', aspect='auto',
             vmax=abs(mats).max(), vmin=-abs(mats).max())
-            plt.show()    
+            plt.show()
 
 
 def add_freq_features(data):
     W = Wavelet(data)
     result = []
-    for i in range(len(data)):
-        d = data[i]
-        w = W[i].flatten()
-        print(d.shape,w.shape)
-        entry = np.append(d,w)
-        print(entry.shape)
-        result.append(entry)
-    return result
+    # for i in range(len(data)):
+    # d = data[i]
+    w = W.flatten()
+    # print(d.shape,w.shape)
+    entry = np.ndarray.tolist(np.append(data,w))
+    # print(entry.shape)
+    # result.append(entry)
+    return entry
 
 def plot_all_features(data):
     mag(data)
-    plot_wavlet(data)    
+    plot_wavlet(data)
     #STFT(data)
-    
+
 #PLOTS GRAPHS OF FREQUENCIES
-plot_all_features(forward_fall)
-add_freq_features(forward_fall)
+# plot_all_features(forward_fall)
+# add_freq_features(forward_fall)
--- svm/svm_fall_detection.py
+++ svm/svm_fall_detection.py
@@ -4,6 +4,8 @@ import math, os, time
 from matplotlib import pyplot as plt
 from matplotlib import patches as patches
 from matplotlib import animation as animation
+from frequency_domain import *
+import sys
 
 # for 10hz sampling rate
 WINDOW_SIZE = 10 # 0.1s interval, so 1s sliding window
@@ -51,84 +53,110 @@ def load_data_from_csv(filepath):
         # max_mag = max(magnitudes[i:i+WINDOW_SIZE])
         # max_ang = max(max(x_ang[i:i+WINDOW_SIZE]), max(y_ang[i:i+WINDOW_SIZE]),
         #               max(z_ang[i:i+WINDOW_SIZE]))
-        window = magnitudes[i:i+WINDOW_SIZE] + z_ang[i:i+WINDOW_SIZE]
+        window = (magnitudes[i:i+WINDOW_SIZE] + z_ang[i:i+WINDOW_SIZE])# +
+                  #x_ang[i:i+WINDOW_SIZE] + y_ang[i:i+WINDOW_SIZE])
+        #+ add_freq_features(magnitudes[i:i+WINDOW_SIZE])
         train_X.append(window)
-        train_Y.append(csv.is_fall[i])
+        # check if there is a fall within the window
+        is_fall = 0
+        for j in range(i, i+WINDOW_SIZE):
+            if csv.is_fall[j]:
+                is_fall = 1
+                break
+        train_Y.append(is_fall)
     return train_X, train_Y
 
-# extract features and break up into windows
-def preprocess_data(filepath):
-    csv = pd.read_csv(filepath, squeeze=True)
-    x_ang, y_ang, z_ang = calculate_angle(csv)
-    magnitudes = calculate_magnitude(csv)
+
+def predict(magnitude, x_ang, y_ang, z_ang):
+    train_X = []
+    train_Y = []
+    for filename in os.listdir(TRAIN_DATA_DIR):
+        if filename.endswith(".csv"):
+            filepath = os.path.join(TRAIN_DATA_DIR, filename)
+            x, y = load_data_from_csv(filepath)
+            train_X.extend(x)
+            train_Y.extend(y)
+    model = svm.SVC()
+    model.fit(train_X, train_Y)
     windows = []
     for i in range(0, len(magnitudes)-WINDOW_SIZE):
         # max_mag = max(magnitudes[i:i+WINDOW_SIZE])
         # max_ang = max(max(x_ang[i:i+WINDOW_SIZE]), max(y_ang[i:i+WINDOW_SIZE]),
         #               max(z_ang[i:i+WINDOW_SIZE]))
-        window = magnitudes[i:i+WINDOW_SIZE] + z_ang[i:i+WINDOW_SIZE]
+        window = (magnitudes[i:i+WINDOW_SIZE] + z_ang[i:i+WINDOW_SIZE])# +
+                 # x_ang[i:i+WINDOW_SIZE] + y_ang[i:i+WINDOW_SIZE])
+        #+ add_freq_features(magnitudes[i:i+WINDOW_SIZE])
         # windows.append((max_mag, max_ang))
         windows.append(window)
-    return windows
-
-# train model with train data
-train_X = []
-train_Y = []
-for filename in os.listdir(TRAIN_DATA_DIR):
-    if filename.endswith(".csv"):
-        filepath = os.path.join(TRAIN_DATA_DIR, filename)
-        x, y = load_data_from_csv(filepath)
-        train_X.extend(x)
-        train_Y.extend(y)
-model = svm.SVC()
-model.fit(train_X, train_Y)
-
-# plot time vs magnitude
-magnitudes = calculate_magnitude(pd.read_csv("./testdata/mixed_1fall.csv"))
-intervals = [0.1 * x for x in range(len(magnitudes))]
-fig = plt.figure()
-plt.plot(intervals, magnitudes)
-plt.xlabel("time (s)")
-plt.ylabel("acceleration")
-ax = plt.gca()
-
-top = max(magnitudes) # top position
-bottom = min(magnitudes)
-width = len(magnitudes) // WINDOW_SIZE
-
-rectangles = []
-# break up data into sliding windows and add rectangles for each sliding window
-for i in range(0, len(magnitudes)-WINDOW_SIZE):
-    rectangles.append(patches.Rectangle((i/WINDOW_SIZE, bottom), 1, top-bottom,
-                      linewidth=1, edgecolor='r', facecolor='none'))
-
-# store texts for each prediction
-windows = []
-windows = preprocess_data("./testdata/mixed_1fall.csv")
-texts = []
-prediction = model.predict(windows)
-for p in prediction:
-    if p == 1:
-        texts.append("fall")
-    else:
-        texts.append("normal")
-
-# draw first rectangle and prediction
-ax.add_patch(rectangles[0])
-ax.set_title(texts[0])
-
-# for updating the sliding window
-def update(i):
-    if len(rectangles) > 1:
-        rectangles[0].remove()
-        rectangles.pop(0)
-        ax.add_patch(rectangles[0])
-        texts.pop(0)
-        ax.set_title(texts[0])
-    return
-
-ani = animation.FuncAnimation(fig, update, interval=50)
-plt.show()
+    prediction = model.predict(windows)
+    return prediction
 
 
+def visualize(magnitudes, prediction):
+
+    # plot time vs magnitude
+    intervals = [0.1 * x for x in range(len(magnitudes))]
+    fig = plt.figure()
+    plt.plot(intervals, magnitudes)
+    plt.xlabel("time (s)")
+    plt.ylabel("acceleration")
+    ax = plt.gca()
+
+    top = max(magnitudes) # top position
+    bottom = min(magnitudes)
+    width = len(magnitudes) // WINDOW_SIZE
+
+    rectangles = []
+    # break up data into sliding windows and add rectangles for each sliding window
+    for i in range(0, len(magnitudes)-WINDOW_SIZE):
+        rectangles.append(patches.Rectangle((i/WINDOW_SIZE, bottom), 1, top-bottom,
+                          linewidth=1, edgecolor='r', facecolor='none', zorder=3))
+
+    # store texts for each prediction
+    texts = []
+    for p in prediction:
+        if p == 1:
+            texts.append("fall")
+        else:
+            texts.append("normal")
+
+    # draw first rectangle and prediction
+    ax.add_patch(rectangles[0])
+    ax.set_title(texts[0])
+
+    running = True
+    # updates the slider
+    def update(i):
+        if len(rectangles) > 1:
+            rectangles[0].remove()
+            rectangles.pop(0)
+            ax.add_patch(rectangles[0])
+            texts.pop(0)
+            ax.set_title(texts[0])
+
+    # starts the animation on mouse click
+    def onClick(event):
+        nonlocal running
+        if running:
+            ani.event_source.stop()
+            running = False
+        else:
+            ani.event_source.start()
+            running = True
+
+    fig.canvas.mpl_connect('button_press_event', onClick)
+    ani = animation.FuncAnimation(fig, update, interval=50)
+    plt.show()
+
+
+
+if __name__ == '__main__':
+    testfile = sys.argv[1]
+    csv = pd.read_csv(testfile, squeeze=True)
+    x_ang, y_ang, z_ang = calculate_angle(csv)
+    magnitudes = calculate_magnitude(csv)
+    prediction = predict(magnitudes, x_ang, y_ang, z_ang)
+    visualize(magnitudes, prediction)
+
+#mixed: fall, fall, walking, jumping
 
